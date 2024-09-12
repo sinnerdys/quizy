75e47fc9f61f9a6d43ab61db1dbd2f622db9e41c
sinnerdys <sinnerdys@gmail.com>
2024-09-12 14:30:14 +0300

    tgup

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ function App() {
       // Устанавливаем кастомный цвет для нижней панели (фон приложения)
       window.Telegram.WebApp.setBackgroundColor('#112558'); // Замени на нужный тебе цвет для нижней панели
 
-      // Отключаем "резиновую" прокрутку
-      window.Telegram.WebApp.disablePullToRefresh(); 
-
       // Отключаем вертикальные свайпы, если необходимо
       window.Telegram.WebApp.disableVerticalSwipes(); 
     }
