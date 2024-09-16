ce44d73a71f4c9ba43c271f7b061216e741077f9
sinnerdys <sinnerdys@gmail.com>
2024-09-16 20:45:23 +0300

    tasks

--- src/components/ModalTask.js
+++ src/components/ModalTask.js
@@ -14,13 +14,23 @@ function ModalTask({ task, onComplete, onClose }) {
       }, 10);
     }
 
+    // Закрытие при нажатии на overlay
+    const handleOverlayClick = (e) => {
+      if (e.target === overlay) {
+        onClose(); // Закрываем модальное окно
+      }
+    };
+
+    overlay.addEventListener('click', handleOverlayClick);
+
     return () => {
       if (overlay && modal) {
         overlay.classList.remove('open');
         modal.classList.remove('open');
+        overlay.removeEventListener('click', handleOverlayClick);
       }
     };
-  }, []);
+  }, [onClose]);
 
   const handleSubscribe = () => {
     window.open(task.subscribeUrl, '_blank');
@@ -33,7 +43,13 @@ function ModalTask({ task, onComplete, onClose }) {
   return (
     <div className="modal-task-overlay">
       <div className="modal-task">
-        <button className="close-button" onClick={onClose}>✕</button>
+        <button className="close-button" onClick={onClose}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icon-tabler-x">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18 6l-12 12" />
+            <path d="M6 6l12 12" />
+          </svg>
+        </button>
         <div className="modal-logo-container">
           <img src={logo} alt="QUIZY Logo" className="modal-logo" />
         </div>
