c6ab3ced0d75678e8646b3383fccf97434506a89
sinnerdys <sinnerdys@gmail.com>
2024-09-21 10:14:55 +0300

    wheel

--- src/components/QuizyWheel.js
+++ src/components/QuizyWheel.js
@@ -26,35 +26,35 @@ const QuizyWheel = () => {
         <g transform="rotate(0)">
         <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M0,-175A175,175 0 0,1 123.744,-123.744L0,0Z"></path>
-      <text id="sector1" transform="rotate(-67.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>500</text>
+      <text id="sector1" transform="rotate(-65) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>500</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M123.744,-123.744A175,175 0 0,1 175,0L0,0Z"></path>
-      <text id="sector2" transform="rotate(-22.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>1000</text>
+      <text id="sector2" transform="rotate(-20) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>1000</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M175,0A175,175 0 0,1 123.744,123.744L0,0Z"></path>
-      <text id="sector3" transform="rotate(22.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>1500</text>
+      <text id="sector3" transform="rotate(25) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>1500</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M123.744,123.744A175,175 0 0,1 0,175L0,0Z"></path>
-      <text id="sector4" transform="rotate(67.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>2000</text>
+      <text id="sector4" transform="rotate(70) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>2000</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M0,175A175,175 0 0,1 -123.744,123.744L0,0Z"></path>
-      <text id="sector5" transform="rotate(112.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>2500</text>
+      <text id="sector5" transform="rotate(115) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>2500</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M-123.744,123.744A175,175 0 0,1 -175,0L0,0Z"></path>
-      <text id="sector6" transform="rotate(157.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>3000</text>
+      <text id="sector6" transform="rotate(160) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>3000</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M-175,0A175,175 0 0,1 -123.744,-123.744L0,0Z"></path>
-      <text id="sector7" transform="rotate(202.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>5000</text>
+      <text id="sector7" transform="rotate(205) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>5000</text>
     </g>
     <g className="slice">
       <path fill="#152A60" stroke="#4365C0" strokeWidth="5" d="M-123.744,-123.744A175,175 0 0,1 0,-175L0,0Z"></path>
-      <text id="sector8" transform="rotate(247.5) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>10000</text>
+      <text id="sector8" transform="rotate(251) translate(120)" textAnchor="middle" style={{ fill: "white", fontSize: "20px" }}>10000</text>
     </g>
   </g>
           {/* Круг в центре */}
