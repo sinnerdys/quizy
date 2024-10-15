7ea0ef90bae1b9d5cf6bcaf080685e5669164f8f
sinnerdys <sinnerdys@gmail.com>
2024-10-15 21:30:18 +0300

    wheel

--- src/components/ModalWin.js
+++ src/components/ModalWin.js
@@ -34,7 +34,7 @@ function ModalWin({ prizeAmount, onClose }) {
   return (
     <div className="modal-win-overlay">
       <div className="modal-win">
-        <button className="close-button" onClick={onClose}>
+        <button className="win-close-button" onClick={onClose}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icon-tabler-x">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M18 6l-12 12" />
