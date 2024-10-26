854b98564e1c581caa09217064f4a39ce5c7e12f
sinnerdys <sinnerdys@gmail.com>
2024-10-26 14:37:07 +0300

    app

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -11,31 +11,31 @@ function BottomNav() {
   return (
     <div className="bottom-nav">
       <NavLink to="/" className="nav-link" activeClassName="active">
-        <div className="menu-icon">
+      <div className="menu-icon active-icon-bg">
           <img src={homeIcon} alt="Home" className="icon-svg" />
         </div>
         <span>Home</span>
       </NavLink>
       <NavLink to="/leaderboard" className="nav-link" activeClassName="active">
-        <div className="menu-icon">
+      <div className="menu-icon active-icon-bg">
           <img src={leaderboardIcon} alt="Leaderboard" className="icon-svg" />
         </div>
         <span>Leaderboard</span>
       </NavLink>
       <NavLink to="/games" className="nav-link" activeClassName="active">
-        <div className="menu-icon">
+      <div className="menu-icon active-icon-bg">
           <img src={gamesIcon} alt="Games" className="icon-svg" />
         </div>
         <span>Games</span>
       </NavLink>
       <NavLink to="/friends" className="nav-link" activeClassName="active">
-        <div className="menu-icon">
+      <div className="menu-icon active-icon-bg">
           <img src={friendsIcon} alt="Friends" className="icon-svg" />
         </div>
         <span>Friends</span>
       </NavLink>
       <NavLink to="/wheel" className="nav-link" activeClassName="active">
-        <div className="menu-icon">
+      <div className="menu-icon active-icon-bg">
           <img src={wheelIcon} alt="Wheel" className="icon-svg" />
         </div>
         <span>Wheel</span>
