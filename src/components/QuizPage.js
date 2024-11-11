4451dfaa866aa0b73cd18a929505720c9b4db246
sinnerdys <sinnerdys@gmail.com>
2024-11-11 22:02:30 +0200

    quizy

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import './QuizPage.css';
 import logo from '../assets/logo.png'; 
