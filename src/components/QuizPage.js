3af6b6763109250c4f6476a84c92cb38306c0ed8
sinnerdys <sinnerdys@gmail.com>
2024-11-12 17:23:37 +0200

    quizy

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -224,7 +224,8 @@ function QuizPage({ userId, onComplete }) {
                 strokeDashoffset={circleProgress}  // Используем обновленный progress
                 style={{
                     transition: 'stroke-dashoffset 0.2s ease-out', // Плавное изменение круга
-                    transform: 'rotate(-90deg)'  // Поворот круга, чтобы начальная точка была сверху
+                    transform: 'rotate(-90deg)',  // Поворот круга, чтобы начальная точка была сверху
+                    transformOrigin: '50% 50%' // Обеспечивает вращение вокруг центра круга
                 }}
             />
         </svg>
